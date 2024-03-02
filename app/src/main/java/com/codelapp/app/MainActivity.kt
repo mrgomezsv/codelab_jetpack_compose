b2851d1adbe9c3eb91bd0e34fbd724572456dfc8
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-01 18:13:39 -0600

    Aplicando estilos y temas a la app

--- app/src/main/java/com/codelapp/app/MainActivity.kt
+++ app/src/main/java/com/codelapp/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelapp.app
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,11 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelapp.app.ui.theme.CodelAppTheme
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodelAppTheme {
                 // A surface container using the 'background' color from the theme
-               MyApp(modifier = Modifier.fillMaxSize())
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -48,26 +49,27 @@ fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
-        if (shouldShowOnboarding) {@Composable
-        fun OnboardingScreen(
-            onContinueClicked: () -> Unit,
-            modifier: Modifier = Modifier
-        ) {
-
-            Column(
-                modifier = modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+        if (shouldShowOnboarding) {
+            @Composable
+            fun OnboardingScreen(
+                onContinueClicked: () -> Unit,
+                modifier: Modifier = Modifier
             ) {
-                Text("Welcome to the Basics Codelab!")
-                Button(
-                    modifier = Modifier.padding(vertical = 24.dp),
-                    onClick = onContinueClicked
+
+                Column(
+                    modifier = modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text("Continue")
+                    Text("Welcome to the Basics Codelab!")
+                    Button(
+                        modifier = Modifier.padding(vertical = 24.dp),
+                        onClick = onContinueClicked
+                    ) {
+                        Text("Continue")
+                    }
                 }
             }
-        }
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
             Greetings()
@@ -145,9 +147,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .weight(1f)
                     .padding(bottom = extraPadding)
-            ){
+            ) {
                 Text(text = "Hello ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
+                ) //Se le aplico stylo a ese texto de los numeros
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -157,6 +162,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)//Preview del modo oscuro
 
 
 @Preview(showBackground = true, widthDp = 320)
